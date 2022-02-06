617eafb36861404a0c49b32f10aea326494423fb
mirzayev-erali <mirzayeverali9368@gmail.com>
2022-02-06 13:31:26 +0500

    #TUGADI

--- script.js
+++ script.js
@@ -21,5 +21,52 @@ fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
     }) .then(displayResults)
 }
 function displayResults(weather) {
-    console.log(weather)
+    console.log(weather);
+    let city = document.querySelector('.location .city');
+    city.innerHTML = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector('.date');
+    date.innerHTML = dateBuilder(now);
+
+    let temp = document.querySelector('.temp');
+    temp.innerHTML = `${weather.main.temp}<span>°C</span>`;
+
+    let condition = document.querySelector('.weather-con');
+    condition.innerHTML = `${weather.weather[0].main}`
+
+    let hiLow = document.querySelector('.hi-low');
+    hiLow.innerHTML = `${weather.main.temp_min}°C / ${weather.main.temp_max}°C`
+
+}
+
+function dateBuilder(s) {
+    let months = [
+        'January',
+'February',
+'March',
+'April',
+'May',
+'June',
+'July',
+'August',
+'September',
+'October',
+'November',
+'December'
+    ];
+    let days = ['Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'];
+
+    let day  = days[s.getDay()];
+    let date  = s.getDate();
+    let month = months[s.getMonth()];
+    let year = s.getFullYear();
+
+    return ` ${day}, ${date} ${month}  ${year} `
 }
